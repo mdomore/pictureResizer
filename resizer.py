fe846727931efc3c13c9b336f9b3b8bd61c5e958
mdomore <michael.moreliere@gmail.com>
2016-03-17 08:33:12 +0100

    Update resizer.py

--- resizer.py
+++ resizer.py
@@ -11,18 +11,25 @@ import glob
 from PIL import Image
 from resizeimage import resizeimage
 
-#Recursively search for JPG files and get the path to them
-for f in glob.glob('./**/*.JPG'):
-    name = os.path.basename(f)      #Get the file name
-    split = f.split('/')            #Split the complete path in parts
+extensions = (".jpg",".jpeg",".JPG",".JPEG")
 
-    img = Image.open(f,)             #Open source image
-    print('Opening '+f)
-    img = resizeimage.resize_thumbnail(img, [1024, 1024])       #Resize the image with a maximum Width or Heigh of 1024
-    if os.path.isfile('owncloud/'+split[1]+'/1024_'+name):
-        print('Image already exists')
-        img.close()
-    else:
-        print ('Saving new file in owncloud/'+split[1]+'/1024_'+name)
-        img.save('owncloud/'+split[1]+'/1024_'+name, img.format)    #Save image to owncloud directory
-        img.close()                     #Close source image
+for ext in extensions:
+    print(ext)
+    #Recursively search for JPG files and get the path to them
+    for f in glob.glob('./**/**/*'+ext):
+        name = os.path.basename(f)      #Get the file name
+        print(os.path.basename(f))
+        split = f.split('/')            #Split the complete path in parts
+        split2 = split[2].split('-')
+        img = Image.open(f,)             #Open source image
+        print('Opening '+f)
+        img = resizeimage.resize_thumbnail(img, [1024, 1024])       #Resize the image with a maximum Width or Heigh of 1024
+        if os.path.isfile('/home/michael/ownCloud/Photos/'+split[1]+'/1024_'+name):
+            print('Image already exists')
+            img.close()
+        else:
+            print ('Saving new file in /home/michael/ownCloud/Photos/'+split2[0]+'_'+split2[1]+'/1024_'+name)
+            img.save('/home/michael/ownCloud/Photos/'+split2[0]+'_'+split2[1]+'/1024_'+name, img.format)    #Save image to owncloud directory
+            img.close()                     #Close source image
+        input("Press Enter to continue...")
+
